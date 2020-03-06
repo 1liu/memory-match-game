0e1859bcaf618b3fdee84964a829a14783f8aa0b
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-03-06 12:59:08 -0800

    Add animation to function buttons

--- main.js
+++ main.js
@@ -1,38 +1,104 @@
 var firstCardClicked;
+var firstCardSibling;
 var secondCardClicked;
+var secondCardSibling
 var firstCardClasses;
 var secondCardClasses;
 var maxMatches = 9;
-var matches=0;
+var matches = 0;
 var gamesPlayed = 0;
 var attempts = 0;
+var mode = 0;
 var accuracy;
 
-shuffleCards();
-var gameCards = document.querySelector("#gameCards");
-gameCards.addEventListener("click", handleClick);
+// Media Queries
+function displayRotate(x) {
+  if (x.matches) { // If media query matches
+    document.querySelector("#start").classList.add("hidden");
+    document.querySelector("#rotateReminder").classList.remove("hidden");
+  } else {
+    document.querySelector("#rotateReminder").classList.add("hidden");
+    document.querySelector("#start").classList.remove("hidden");
+  }
+}
 
+var x = window.matchMedia("(max-width: 560px)")
+displayRotate(x) // Call listener function at run time
+x.addListener(displayRotate) // Attach listener function on state changes
+
+//
+
+shuffleCards();
+//Mode Selection
+var easy = document.querySelector("#button-easy");
+easy.addEventListener("click", function () {
+  mode = 1;
+  easy.classList.add("red");
+  normal.classList.remove("red");
+  hard.classList.remove("red");
+});
+var normal = document.querySelector("#button-normal");
+normal.addEventListener("click", function () {
+  mode = 2;
+  normal.classList.add("red");
+  easy.classList.remove("red");
+  hard.classList.remove("red");
+});
+var hard = document.querySelector("#button-hard");
+hard.addEventListener("click", function () {
+  mode = 3;
+  hard.classList.add("red");
+  easy.classList.remove("red");
+  normal.classList.remove("red");
+});
+
+//Start Function
+var startButton = document.querySelector("#button-start");
+startButton.addEventListener("click", startGame);
+
+var gameCards = document.querySelector(".gameCards");
+gameCards.addEventListener("click", handleClick);
+// console.log("event:",event);
+//Reset Function
+var reset = document.querySelector("#reset");
+reset.addEventListener("click",resetGame);
+
+//Hint Function
+var hint = document.querySelector("#hint");
+hint.addEventListener("click", hintFunction);
+
+// Handle Function
 function handleClick(event) {
   if (event.target.className.indexOf("card-back") === -1) {
     return;
   }
+  console.log("event:", event);
+
   var theTarget = event.target;
   theTarget.classList.add("hidden");
 
   if (!firstCardClicked) {
     firstCardClicked = event.target;
-    firstCardClasses = event.target.previousElementSibling.className;
+    firstCardSibling = firstCardClicked.previousElementSibling;
+    firstCardClasses = firstCardSibling.className;
+    firstCardSibling.classList.remove("hidden");
+    setTimeout(hideFirstCard, 2000 - mode * 600);
   }
   else {
     secondCardClicked = event.target;
-    secondCardClasses = event.target.previousElementSibling.className;
+    secondCardSibling = event.target.previousElementSibling;
+    secondCardClasses = secondCardSibling.className;
+    secondCardSibling.classList.remove("hidden");
     gameCards.removeEventListener("click", handleClick);
 
     if (firstCardClasses === secondCardClasses) {
       matches++;
+      showFirstCard();
       if (matches === maxMatches) {
         console.log("attempts:", attempts);
         document.querySelector("#win").classList.remove("hidden");
+        // reset mode here
+        mode = 0;
       }
       firstCardClicked = null;
       secondCardClicked = null;
@@ -40,7 +106,7 @@ function handleClick(event) {
     }
     else {
       console.log("attempts:", attempts);
-      setTimeout(hide,1500);
+      setTimeout(hideBoth, 2000-mode*600);
     }
     attempts++;
     displayStats();
@@ -49,39 +115,51 @@ function handleClick(event) {
 var button = document.querySelector("#button");
 button.addEventListener("click", resetGame);
 
-function hide() {
+function hideBoth() {
   firstCardClicked.classList.remove("hidden");
   secondCardClicked.classList.remove("hidden");
+
+  firstCardSibling.classList.add("hidden");
+  secondCardSibling.classList.add("hidden");
   firstCardClicked = null;
   secondCardClicked = null;
   gameCards.addEventListener("click", handleClick);
 }
+function hideFirstCard(){
+  firstCardClicked.classList.remove("hidden");
+  firstCardSibling.classList.add("hidden");
+}
 
-function displayStats(){
+function displayStats() {
   document.querySelector("#gamesPlayed").textContent = gamesPlayed;
   document.querySelector("#attempts").textContent = attempts;
-  document.querySelector("#accuracy").textContent = calculateAccuracy(attempts,matches)+"%";
+  document.querySelector("#accuracy").textContent = calculateAccuracy(attempts, matches) + "%";
 }
 
-function calculateAccuracy(attempts, matches){
-  if(attempts === 0){
+function calculateAccuracy(attempts, matches) {
+  if (attempts === 0) {
     return 0;
   }
-  return Math.trunc(matches/attempts*100);
+  return Math.trunc(matches / attempts * 100);
 
 }
 
-function resetGame(){
+function resetGame() {
   resetCards();
   matches = 0;
   gamesPlayed = 0;
   attempts = 0;
   accuracy = 0;
+  // mode = 0;
   displayStats();
   document.querySelector("#win").classList.add("hidden");
+  document.querySelector("#start").classList.add("hidden");
+  easy.classList.remove("red");
+  normal.classList.remove("red");
+  hard.classList.remove("red");
 }
 
-function resetCards(){
+function resetCards() {
   var x = document.querySelectorAll(".card-back");
   for (let i = 0; i < x.length; i++) {
     x[i].classList.remove("hidden");
@@ -89,9 +167,9 @@ function resetCards(){
 
 }
 
-function shuffleCards(){
+function shuffleCards() {
   var cards = document.querySelectorAll(".card-front");
-  for (let i = 0; i<cards.length;i++){
+  for (let i = 0; i < cards.length; i++) {
     var randomPosition = Math.floor(Math.random() * cards.length)
     var placeHolder = cards[i].className;
     cards[i].className = cards[randomPosition].className;
@@ -99,4 +177,34 @@ function shuffleCards(){
 
   }
 
+}
+
+function startGame() {
+  if (mode) {
+    resetGame();
+    document.querySelector(".container").classList.remove("no-click");
+  }
+}
+
+function hintFunction(){
+  console.log("Hint Clicked");
+  flipAll();
+  setTimeout(flipAll, 100);
+}
+
+function flipAll(){
+  var allCards = document.querySelectorAll(".card-front,.card-back");
+  for (let i =0; i<allCards.length;i++){
+    if(allCards[i].classList.contains("hidden")){
+      allCards[i].classList.remove("hidden");
+    }
+    else{
+      allCards[i].classList.add("hidden");
+    }
+  }
+}
+
+function showFirstCard(){
+  firstCardClicked.classList.add("hidden");
+  firstCardSibling.classList.remove("hidden");
 }
